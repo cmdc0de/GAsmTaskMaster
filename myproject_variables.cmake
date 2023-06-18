7ec259dfc7fdf784cb5f85b28f31908d7acd271e
cmdc0de <cmdc0dez@gmail.com>
2023-06-18 15:39:06 -0700

    adding server directory and testing changes to the template project

--- myproject_variables.cmake
+++ myproject_variables.cmake
@@ -1,11 +1,11 @@
 set(PROJ_EXEC1 "intro")
+set(SERVER_APP "g_asm_task_mgr")
 
 #directories under src that will be added as subdirectories to the project
-set(MY_PROJECT_SUBDIRECTORIES "ftxui_sample;sample_library")
+set(MY_PROJECT_SUBDIRECTORIES "server;ftxui_sample;sample_library")
 
 set(DEFAULT_EXEC ${PROJ_EXEC1} CACHE STRING "Default executable to build")
 
-#set(MY_PROJECT_TARGETS "${PROJ_EXEC1} ${SERVER_APP}")
-set(MY_PROJECT_TARGETS "${PROJ_EXEC1}")
+set(MY_PROJECT_TARGETS "${PROJ_EXEC1} ${SERVER_APP}")
 
 MESSAGE(STATUS "MY PROJECT TARGETS = ${MY_PROJECT_TARGETS}")
